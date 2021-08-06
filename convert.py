b70de105ae95c294875da2d806df82500af0a000
patel-zeel <patel_zeel@iitgn.ac.in>
2021-08-06 13:40:13 +0530

    ok

--- convert.py
+++ convert.py
@@ -3,12 +3,12 @@ import re
 
 os.system(
     "jupyter nbconvert index_dev.ipynb  --TemplateExporter.exclude_input=True --to html")
-path = 'index_dev.html'
-save_path = 'index.html'
-with open(path, 'r') as f:
-    data = f.read()
-data = re.sub('Out\[\d+\]:', '', data)
-data = re.sub('<title>.*</title>',
-              '<title>Active Learning: A Visual Tour</title>', data)
-with open(save_path, 'w') as f:
-    f.write(data)
+# path = 'index_dev.html'
+# save_path = 'index.html'
+# with open(path, 'r') as f:
+#     data = f.read()
+# data = re.sub('Out\[\d+\]:', '', data)
+# data = re.sub('<title>.*</title>',
+#               '<title>Intuitive Explanation of Non-stationary Gaussian Process Kernels</title>', data)
+# with open(save_path, 'w') as f:
+#     f.write(data)
